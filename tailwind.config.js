6a90c65be56299559d5965770543a9dc4b9d450a
weirdolando <93187436+weirdolando@users.noreply.github.com>
2023-03-17 13:58:32 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [
+    require("./src/plugins/openVariant"),
+    require("./src/plugins/animationDelay"),
+    require("./src/plugins/tableCaption"),
+  ],
+};
